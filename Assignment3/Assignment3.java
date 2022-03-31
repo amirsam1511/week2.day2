4988ebc2aea755d4bbae47a1f9713390a1da7594
amirsam1511 <101941368+amirsam1511@users.noreply.github.com>
2022-03-31 16:55:06 +0530

    Create Assignment3.java

---
+++ Assignment3/Assignment3.java
@@ -0,0 +1,27 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[text() = 'Log Out']")).click();
+		driver.close();
+	}
+
+}
